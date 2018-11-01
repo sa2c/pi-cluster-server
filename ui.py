55fb58cb111878aed1f6aa60cde8a9003662bb6e
Mark Dawson <mark.dawson@swansea.ac.uk>
2018-11-01 10:39:01 +0000

    blank leaderboard

--- ui.py
+++ ui.py
@@ -16,41 +16,7 @@ if __name__ == '__main__':
     # initialise another thread for video capture
     th = VideoCaptureThread()
 
-    data = np.load('kinect_to_points/color_kinect_data.npy')
-    depths = np.load('kinect_to_points/kinect_data.npy')
-    depthimages = [depth_to_depthimage(depth) for depth in depths]
-    simulations = {
-        '23454325': {
-            'name': 'Bob Jones',
-            'score': 10.5,
-            'time': '10:00 12/15/2018',
-            'rgb_frame': data[0],
-            'depth_frame': depthimages[0]
-        },
-        '3445345': {
-            'name': 'Terry Berry',
-            'score': 9.5,
-            'time': '11:15 12/15/2018',
-            'rgb_frame': data[1],
-            'depth_frame': depthimages[1]
-        },
-        '234523452': {
-            'name': 'Bob Jones',
-            'score': 10.5,
-            'time': '10:00 12/15/2018',
-            'rgb_frame': data[0],
-            'depth_frame': depthimages[0]
-        },
-        '23452345': {
-            'name': 'Terry Berry',
-            'score': 9.5,
-            'time': '11:15 12/15/2018',
-            'rgb_frame': data[1],
-            'depth_frame': depthimages[1]
-        }
-    }
-
-    window = ControlWindow(simulations)
+    window = ControlWindow({})
 
     th.changeFramePixmap.connect(window.ui.video_rgb.setImage)
     th.changeFramePixmap.connect(window.viewfinder.main_video.setImage)
