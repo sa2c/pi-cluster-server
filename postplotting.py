f50fcefd8726bccc41ada2ad011dacd8f8954054
Mark Dawson <mark.dawson@swansea.ac.uk>
2018-11-01 11:42:10 +0000

    postplotting updates

--- postplotting.py
+++ postplotting.py
@@ -10,7 +10,8 @@ from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 import cv2, sys
 
-def vtkfile_to_numpy(filename,nprocs):
+
+def vtkfile_to_numpy(filename, nprocs):
     # Open the file with read only permit
     vtkfile = open(filename, "r")
 
@@ -29,15 +30,15 @@ def vtkfile_to_numpy(filename,nprocs):
     numpoints = int(listtemp[1])
     print("numpoints=", numpoints)
     # Point coordinates
-    coords = np.zeros((numpoints,3), dtype=float)
+    coords = np.zeros((numpoints, 3), dtype=float)
     for ii in range(numpoints):
         line = vtkfile.readline()
         #print("Line {}: {}".format(ii, line.strip()))
         listtemp = " ".join(line.split())
         listtemp = listtemp.split(" ")
-        coords[ii,0] = float(listtemp[0])
-        coords[ii,1] = float(listtemp[1])
-        coords[ii,2] = float(listtemp[2])
+        coords[ii, 0] = float(listtemp[0])
+        coords[ii, 1] = float(listtemp[1])
+        coords[ii, 2] = float(listtemp[2])
 
     # Elements(Cells)
     line = vtkfile.readline()
@@ -46,15 +47,15 @@ def vtkfile_to_numpy(filename,nprocs):
     numcells = int(listtemp[1])
     print("numcells=", numcells)
     # Elements connectivity
-    elems = np.zeros((numcells,3), dtype=int)
+    elems = np.zeros((numcells, 3), dtype=int)
     for ii in range(numcells):
         line = vtkfile.readline()
         #print("Line {}: {}".format(ii, line.strip()))
         listtemp = " ".join(line.split())
         listtemp = listtemp.split(" ")
-        elems[ii,0] = int(listtemp[1])
-        elems[ii,1] = int(listtemp[2])
-        elems[ii,2] = int(listtemp[3])
+        elems[ii, 0] = int(listtemp[1])
+        elems[ii, 1] = int(listtemp[2])
+        elems[ii, 2] = int(listtemp[3])
 
     # CELL_TYPES
     line = vtkfile.readline()
@@ -64,7 +65,7 @@ def vtkfile_to_numpy(filename,nprocs):
     for ii in range(iii):
         line = vtkfile.readline()
 
-    if(nprocs > 1):
+    if (nprocs > 1):
         # CELL DATA - coloring
         line = vtkfile.readline()
         line = vtkfile.readline()
@@ -78,89 +79,106 @@ def vtkfile_to_numpy(filename,nprocs):
     line = vtkfile.readline()
 
     # Pressure
-    pressure = np.zeros((numpoints,1), dtype=float)
+    pressure = np.zeros((numpoints, 1), dtype=float)
     for ii in range(numpoints):
         line = vtkfile.readline()
         #print("Line {}: {}".format(ii, line.strip()))
         listtemp = " ".join(line.split())
         listtemp = listtemp.split(" ")
-        pressure[ii,0] = float(listtemp[0])
+        pressure[ii, 0] = float(listtemp[0])
 
     # Velocity
     line = vtkfile.readline()
 
-    velocity = np.zeros((numpoints,3), dtype=float)
+    velocity = np.zeros((numpoints, 3), dtype=float)
     for ii in range(numpoints):
         line = vtkfile.readline()
         #print("Line {}: {}".format(ii, line.strip()))
         listtemp = " ".join(line.split())
         listtemp = listtemp.split(" ")
-        velocity[ii,0] = float(listtemp[0])
-        velocity[ii,1] = float(listtemp[1])
-        velocity[ii,2] = float(listtemp[2])
+        velocity[ii, 0] = float(listtemp[0])
+        velocity[ii, 1] = float(listtemp[1])
+        velocity[ii, 2] = float(listtemp[2])
 
     vtkfile.close()
 
-    return coords, elems,velocity
+    return coords, elems, velocity
 
-def plot(canvas,coords,elems,velocity,dotri,dovector,docontour,subject_image,
-        velo_magn_max = None):
+
+def plot(canvas,
+         coords,
+         elems,
+         velocity,
+         dotri,
+         dovector,
+         docontour,
+         subject_image,
+         velo_magn_max=None):
 
     fig = canvas.figure
     ax = fig.axes[0]
     ax.axis('off')
 
-    xplotlims = (min(coords[:,0]),max(coords[:,0]))
-    yplotlims = (min(coords[:,1]),max(coords[:,1]))
+    xplotlims = (min(coords[:, 0]), max(coords[:, 0]))
+    yplotlims = (min(coords[:, 1]), max(coords[:, 1]))
 
     if subject_image is not None:
-        xplotlim_m = max(min(coords[:,0]),0)
-        xplotlim_p = min(max(coords[:,0]),subject_image.shape[1])
+        xplotlim_m = max(min(coords[:, 0]), 0)
+        xplotlim_p = min(max(coords[:, 0]), subject_image.shape[1])
 
-        yplotlim_m = max(min(coords[:,1]),0)
-        yplotlim_p = min(max(coords[:,1]),subject_image.shape[0])
+        yplotlim_m = max(min(coords[:, 1]), 0)
+        yplotlim_p = min(max(coords[:, 1]), subject_image.shape[0])
 
-        xplotlims = (xplotlim_m,xplotlim_p)
-        yplotlims = (yplotlim_m,yplotlim_p)
+        xplotlims = (xplotlim_m, xplotlim_p)
+        yplotlims = (yplotlim_m, yplotlim_p)
     else:
-        xplotlims = (min(coords[:,0]),max(coords[:,0]))
-        yplotlims = (min(coords[:,1]),max(coords[:,1]))
+        xplotlims = (min(coords[:, 0]), max(coords[:, 0]))
+        yplotlims = (min(coords[:, 1]), max(coords[:, 1]))
 
     ax.set_xlim(xplotlims)
     ax.set_ylim(yplotlims)
 
-    # In any case we plot the mesh 
+    # In any case we plot the mesh
     if dotri:
-        ax.triplot(coords[:,0], coords[:,1], elems, color='red', linewidth=0.4)
+        ax.triplot(
+            coords[:, 0], coords[:, 1], elems, color='red', linewidth=0.4)
 
     if docontour:
-        velocity_magn = np.hypot(velocity[:,0],velocity[:,1])
+        velocity_magn = np.hypot(velocity[:, 0], velocity[:, 1])
         if velo_magn_max is None:
             velo_magn_max = velocity_magn.max()
-        VV=np.linspace(0.0, velo_magn_max, 20)
+        VV = np.linspace(0.0, velo_magn_max, 20)
 
         mappable = ax.tricontourf(coords[:,0], coords[:,1], elems, \
                 velocity_magn, VV, cmap="rainbow", extend='both')
-        
-        # this must go on another picture or it breaks the reference frame for 
+
+        # this must go on another picture or it breaks the reference frame for
         # the subject image
-        # plt.figure( 
-        # plt.colorbar(mappable) 
+        # plt.figure(
+        # plt.colorbar(mappable)
         # all the operations to save the colorbar in another picture
 
     if dovector:
-        ax.quiver(coords[:,0], coords[:,1], velocity[:,0], velocity[:,1], 
-                angles='xy', scale_units='xy',color = 'lightgreen',width=0.005,scale=0.0325)
+        ax.quiver(
+            coords[:, 0],
+            coords[:, 1],
+            velocity[:, 0],
+            velocity[:, 1],
+            angles='xy',
+            scale_units='xy',
+            color='lightgreen',
+            width=0.005,
+            scale=0.0325)
 
     fig.canvas.draw()
 
     if subject_image is not None:
         #https://matplotlib.org/gallery/misc/agg_buffer_to_array.html
-        dypx,dxpx,_ = np.array(fig.canvas.renderer._renderer).shape
-        subject_image = subject_image[:,:,[2,1,0]]
-        M = np.float32([[1,0,0],[0,-1,dypx]])
+        dypx, dxpx, _ = np.array(fig.canvas.renderer._renderer).shape
+        subject_image = subject_image[:, :, [2, 1, 0]]
+        M = np.float32([[1, 0, 0], [0, -1, dypx]])
 
-        subject_layer = cv2.warpAffine(subject_image,M,(dxpx,dypx))
+        subject_layer = cv2.warpAffine(subject_image, M, (dxpx, dypx))
 
         ax.imshow(subject_layer)
 
@@ -170,19 +188,17 @@ def plot(canvas,coords,elems,velocity,dotri,dovector,docontour,subject_image,
 def vtk_to_plot(canvas, vtk_filename, nprocs, dotri,dovector,docontour,subject_image,\
         velocity_magn=None):
 
-
-    coords, elems,velocity = vtkfile_to_numpy(vtk_filename,nprocs)
-    return plot(canvas, coords,elems,velocity,dotri,dovector,docontour,subject_image,
-            velocity_magn)
-
+    coords, elems, velocity = vtkfile_to_numpy(vtk_filename, nprocs)
+    return plot(canvas, coords, elems, velocity, dotri, dovector, docontour,
+                subject_image, velocity_magn)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = PlotCanvas()
-    vtk_file = 'playground/scf1/mesh/elmeroutput0001.vtk'
-    image_file = 'playground/kinect/scf1-fullcolorimage.png'
+    vtk_file = 'outbox/testrun1/elmeroutput0001.vtk'
+    image_file = 'outbox/testrun1/kinect/scf1-fullcolorimage.png'
     image = plt.imread(image_file)
-    vtk_to_plot(window, vtk_file, 1, False, True, False, image )
+    vtk_to_plot(window, vtk_file, 1, False, True, False, image)
     window.show()
     sys.exit(app.exec_())
