5c84ec9fd80e96a74380c04488be8d9d1fd688d2
Mark Dawson <mark.dawson@swansea.ac.uk>
2018-11-02 19:05:12 +0000

    activity monitor ip change

--- activity_monitor.py
+++ activity_monitor.py
@@ -10,6 +10,7 @@ import time
 already_set_up = False
 cluster_address = "pi@10.0.0.253"
 
+
 def setup(frontend):
     ''' Copies cpuloadinfo.sh to the cluster'''
     global already_set_up
@@ -20,14 +21,18 @@ def setup(frontend):
 
 def fetch_from_ssh():
     global already_set_up
-    frontend = Connection(host=cluster_address,connect_kwargs={"password":"sa2cpi"})
+    frontend = Connection(
+        host=cluster_address, connect_kwargs={"password": "sa2cpi"})
     if not already_set_up:
         setup(frontend)
     output = frontend.run('''bash cpuloadinfo.sh''', hide=True).stdout
     cpu_usage = output.split('\n')[1:-1]
-    cpu_usage = [float(cpu_usage_meas.split(' ')[1]) for cpu_usage_meas in cpu_usage]
+    cpu_usage = [
+        float(cpu_usage_meas.split(' ')[1]) for cpu_usage_meas in cpu_usage
+    ]
     cpu_usage = np.array(cpu_usage)
     return cpu_usage
+
 
 class ActivityPlotter(PlotCanvas):
     def __init__(self, parent=None):
@@ -36,27 +41,23 @@ class ActivityPlotter(PlotCanvas):
         a.activity_updated.connect(self.update_plot)
         a.start()
 
-
-
     @Slot(np.ndarray)
     def update_plot(self, cpu_usage):
 
-        ax = self.figure.add_axes([0,0,1,1])
+        ax = self.figure.add_axes([0, 0, 1, 1])
         ax.clear()
-        ax.set_ylim([0,100])
-
+        ax.set_ylim([0, 100])
 
         # gets the CPU usage from all the nodes and plots the data
-        num_nodes  = len(cpu_usage)
+        num_nodes = len(cpu_usage)
 
         #cpu_usage_global = np.zeros((timeperiod,num_nodes), dtype=float)
 
-        nodes = np.linspace(1,num_nodes,num_nodes)
+        nodes = np.linspace(1, num_nodes, num_nodes)
         ax.bar(nodes, cpu_usage)
         ax.set_xlabel("Node number")
         ax.set_ylabel("CPU usage (%)")
         self.draw()
-
 
     def redraw_plot(self):
         self.figure.canvas.draw()
