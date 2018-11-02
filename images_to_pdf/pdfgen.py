058c607b0154a4be63810d699000fa3daf5af3f1
Ed Bennett <E.J.Bennett@swansea.ac.uk>
2018-11-02 15:38:07 +0000

    allow generating of PDFs from numpy arrays in a separate thread

--- images_to_pdf/pdfgen.py
+++ images_to_pdf/pdfgen.py
@@ -2,6 +2,13 @@
 
 from reportlab.pdfgen import canvas
 from reportlab.platypus import Paragraph, KeepInFrame
+from reportlab.lib.utils import ImageReader
+
+from PySide2.QtCore import QRunnable
+from PIL import Image
+from io import BytesIO
+from numpy import uint8
+from subprocess import call
 
 from brand import erdf_logo, scw_logo, scw_bg, get_styles
 
@@ -71,9 +78,54 @@ def build_document(output_filename,
     page.save()
 
 
+class PDFGenerator(QRunnable):
+    def __init__(self,
+                 output_filename,
+                 image_array_top_left, image_array_top_right,
+                 image_array_bottom_left, image_array_bottom_right,
+                 visitor_name, visitor_score):
+        self.output_filename = output_filename
+        self.image_arrays = [
+            image_array_top_left,
+            image_array_top_right,
+            image_array_bottom_left,
+            image_array_bottom_right
+        ]
+        self.visitor_name = visitor_name
+        self.visitor_score = visitor_score
+
+    def run(self):
+        images = []
+        for image_array in self.image_arrays:
+            image_io = BytesIO()
+            Image.fromarray(uint8(image_array)).save(image_io, format='JPEG')
+            image_io.seek(0)
+            images.append(ImageReader(image_io))
+
+        build_document(
+            self.output_filename,
+            *images,
+            self.visitor_name,
+            self.visitor_score
+        )
+        call(['lpr', '-o', 'landscape', self.output_filename])
+
+
 if __name__ == "__main__":
-    build_document(
-        'test.pdf',
-        'test1.jpg', 'test2.jpg', 'test3.jpg', 'test4.jpg',
-        'Test User', 100
-    )
+    # build_document(
+    #     'test.pdf',
+    #     'test1.jpg', 'test2.jpg', 'test3.jpg', 'test4.jpg',
+    #     'Test User', 100
+    # )
+    import matplotlib.pyplot as plt
+
+    image1 = plt.imread('test1.jpg')
+    image2 = plt.imread('test2.jpg')
+    image3 = plt.imread('test3.jpg')
+    image4 = plt.imread('test4.jpg')
+
+    generator = PDFGenerator('test_pil.pdf',
+                             image1, image2, image3, image4,
+                             'Test user with PIL', 69)
+#    import pdb; pdb.set_trace()
+    generator.run()
