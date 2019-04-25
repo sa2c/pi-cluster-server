498460c20990597cf56094f49509b20df6ee179b
Jarno Rantaharju <j.m.o.rantaharju@swansea.ac.uk>
2019-04-25 14:16:45 +0100

    Kinect and contour settings in settings.py

--- settings.py
+++ settings.py
@@ -1,2 +1,15 @@
 
+# Cluster settings
 cluster_address = "jarno@localhost"
+
+# Kinect depth data settigs
+dmin = 1000
+dmax = 3000
+min_distance = 230  # out of 255
+
+# Settings related to the contour estimation
+num_points = 100
+
+# Image display adjustments
+color_scale = [1, 1, 0.8]
+flip_display_axis = True
