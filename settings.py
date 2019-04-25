3e56744cf762361e0cbbb8c8fa5607432590a2fb
Jarno Rantaharju <j.m.o.rantaharju@swansea.ac.uk>
2019-04-25 14:50:45 +0100

    Move more kinect settings to settings.py

--- settings.py
+++ settings.py
@@ -2,6 +2,9 @@
 # Cluster settings
 cluster_address = "jarno@localhost"
 
+# Mock Kinect
+mock_kinect = True
+
 # Kinect depth data settigs
 dmin = 1000
 dmax = 3000
@@ -9,6 +12,7 @@ min_distance = 230  # out of 255
 
 # Settings related to the contour estimation
 num_points = 100
+corner_cutting_steps = 50
 
 # Image display adjustments
 color_scale = [1, 1, 0.8]
