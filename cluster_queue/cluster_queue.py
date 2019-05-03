ce94835c92eb1f3260a833ec3027f5624dc07955
j.m.o.rantaharju <j.m.o.rantaharju@swansea.ac.uk>
2019-05-03 16:11:49 +0100

    Queue implemented in python

--- cluster_queue/cluster_queue.py
+++ cluster_queue/cluster_queue.py
@@ -1,5 +1,7 @@
 import os, sys
 import subprocess
+import time
+import shutil
 
 from settings import *
 
@@ -14,6 +16,7 @@ def reserve_nodes( n_nodes ):
 	    my_nodes.add(node)
     return my_nodes
 
+
 def write_hostfile( nodes, id ):
     hostfilename="hostfile_"+id
     with open(hostfilename, "w") as f:
@@ -23,25 +26,79 @@ def write_hostfile( nodes, id ):
             f.write(line)
     return hostfilename
 
+
 def nodes_available():
     return( len(free_nodes) )
 
+
 def run_cfd( id ):
     my_nodes = reserve_nodes( nodes_per_job )
+    os.makedirs('outbox/'+id, exist_ok=True)
     os.chdir('cfd')
     hostfilename = write_hostfile( my_nodes, id )
-    command = "python runcfd.py {} {} {} >> ../outbox/$id/output".format(
-        id,
-        nodes_per_job*nslots,
-        hostfilename
+    command = cfdcommand.format(
+        id=id,
+        ncores=nodes_per_job*nslots,
+        hostfile=hostfilename
     )
-    subprocess.run(command, shell=True)
+    command = ["sleep","10"]
+    process = subprocess.Popen(command)
+    os.chdir(local_path)
+    return process
 
+
+def create_file(filename):
+    open(filename, 'a').close()
+
+
+def check_signals():
+    slot = 1
+    signals = os.listdir('signal')
+    if( len(signals) > 0):
+        signal = signals[0]
+        print("Starting", signal)
+        try:
+            os.remove('signal/'+signal)
+        except:
+            return []
+
+        create_file("signal_out/{}_start_{}".format(signal,slot))
+        shutil.copyfile(
+            'inbox/'+signal,
+            'cfd/'+signal+'-outline-coords.dat'
+        )
+        run = run_cfd(signal)
+        return [(run,signal,slot)]
+    else:
+        return []
+
+
+def run_queue():
+    runs = []
+    while True:
+        os.chdir(local_path)
+        if nodes_available() >= nodes_per_job :
+            runs += check_signals()
+        time.sleep(1)
+        
+        for run in runs:
+            process, signal, slot = run
+            if process.poll() is not None:
+                print("finished", signal)
+                create_file("signal_out/{}_end_{}".format(signal,slot))
+                runs.remove(run)
+
+
+
+
+
+if __name__ == '__main__':
+    run_queue()
 
 
 print(free_nodes)
 my_nodes = reserve_nodes(2)
 print(my_nodes)
-print(write_hostfile( my_nodes, "id"))
+print(write_hostfile( my_nodes, "test"))
 print(free_nodes)
-print(run_cfd( "id" ))
+print(run_cfd( "test" ))
--- cluster_queue/settings.py
+++ cluster_queue/settings.py
@@ -9,6 +9,9 @@ nodes_per_job = 1
 
 IPs = [ "127.0.0.1" for i in range(nnodes) ]
 
+cfdcommand = "python runcfd.py {id} {ncores} {hostfile} >> ../outbox/{id}/output"
+cfdcommand = "sleep 10 >> ../outbox/{id}/output"
+
 os.makedirs('signal_out', exist_ok=True)
 os.makedirs('signal', exist_ok=True)
 os.makedirs('inbox', exist_ok=True)
