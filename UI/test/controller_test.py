c73fd7bb5986d4e1da8ddf52c53bd2d44c88297e
j.m.o.rantaharju <j.m.o.rantaharju@swansea.ac.uk>
2019-05-15 17:33:24 +0100

    Controller tests

--- UI/test/controller_test.py
+++ UI/test/controller_test.py
@@ -13,6 +13,26 @@ class TestController(object):
     def test_init(self):
         assert self.controller.offset == [0, 0]
         assert self.controller.scale == [1.0, 1.0]
+        assert len(self.controller.contour.shape) == 2
 
         assert isinstance(self.controller.current_name, str)
 
+        assert self.controller.background.shape == (480, 640)
+
+    def test_calibrate(self):
+        self.controller.background = None
+        self.controller.calibrate()
+
+        assert self.controller.background.shape == (480, 640)
+
+    def test_capture(self):
+        frame, depth_rgb = self.controller.capture()
+
+        assert frame.shape == (480, 640, 3)
+        assert depth_rgb.shape == (480, 640, 3)
+
+    def get_user_details(self):
+        frame, depth_rgb = self.controller.capture()
+
+        assert frame.shape == (480, 640, 3)
+        assert depth_rgb.shape == (480, 640, 3)
