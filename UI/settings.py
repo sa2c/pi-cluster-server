18c3116ac5020a5492183ffe8c6bbcccf92ab1e7
j.m.o.rantaharju <j.m.o.rantaharju@swansea.ac.uk>
2019-06-14 10:52:17 +0100

    Better default settings

--- UI/settings.py
+++ UI/settings.py
@@ -2,16 +2,16 @@ import os
 from tempfile import mkdtemp
 
 # Mock Kinect
-mock_kinect = True
+mock_kinect = False
 
 # Kinect depth data settigs
-dmin = 1000
-dmax = 3000
+dmin = 0
+dmax = 1000
 min_distance = 230  # out of 255
 
 # Settings related to the contour estimation
 num_points = 100
-nmeasurements = 5
+nmeasurements = 20
 corner_cutting_steps = 10
 
 # Image display adjustments
@@ -19,8 +19,8 @@ color_scale = [1, 1, 0.8]
 flip_display_axis = True
 
 # Cluster settings
-cluster_address = "localhost"
-cluster_path = os.environ['PWD']+'/../cluster_queue'
+cluster_address = "pi@10.0.0.253"
+cluster_path = 'picluster/cluster_queue'
 
 # Local settings
 local_path = os.environ['PWD']
