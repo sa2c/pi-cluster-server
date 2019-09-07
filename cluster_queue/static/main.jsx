cbc0a7e03f25e134c85fe03b19d1ba9b104ea750
Mark Dawson <mark.dawson@swansea.ac.uk>
2019-09-07 08:14:35 +0100

    added leaderboard and recent

--- cluster_queue/static/main.jsx
+++ cluster_queue/static/main.jsx
@@ -1,5 +1,6 @@
 function MainPanel(props) {
-    const img_src = "simulations/" + props.currentSimulation + "/elmeroutput0001-velomagn.png"
+  const img_src = "simulations/" + props.currentSimulation +
+    "/elmeroutput0001-velomagn.png"
 
   return (
     <img id="result-main-image"
@@ -12,34 +13,43 @@ function MainPanel(props) {
 function SimulationList(props) {
   return (
     props.simulations.map((sim_id, rank) => {
-        const image_url = "simulations/" + sim_id +
-            "/elmeroutput0001-velomagn.png";
-        const image_alt = "Simulation " + sim_id + " image";
-        const activeClass = props.currentSimulation == sim_id ? "selected" : "";
+
+      const image_url = "simulations/" + sim_id +
+        "/elmeroutput0001-velomagn.png";
+
+      const image_alt = "Simulation " + sim_id + " image";
+
+      const additionalMainClasses =
+        props.currentSimulation == sim_id ? " selected" : "";
+
+      const additionalRankingClasses = props.showIndex ? "" : " hidden";
+
       return (
-          <div className={"columns simulation " + activeClass}
+        <div className={"columns simulation" + additionalMainClasses}
                      key={rank}
                      id={"rank" + (sim_id + 1)}
                      onClick={ () => props.onClick(sim_id) }>
 
-                  <div className="ranking column is-one-fifth">
-                    <h2>
-                      { rank + 1 }
-                    </h2>
-                  </div>
+            <div
+              className={"ranking column is-one-fifth" + additionalRankingClasses}
+            >
+            <h2>
+              { rank + 1 }
+            </h2>
+          </div>
 
-                  <div className="simulation-data">
+          <div className="simulation-data">
 
-                    <img src={image_url}
-                         alt={image_alt}
-                         width="100%" />
+            <img src={image_url}
+                 alt={image_alt}
+                 width="100%" />
 
-                    <div className="simulation-info">
-                      <p>Mark</p>
-                      <p>Drag: 1.3354</p>
-                    </div>
-                  </div>
-                </div>
+            <div className="simulation-info">
+              <p>Mark</p>
+              <p>Drag: 1.3354</p>
+            </div>
+          </div>
+          </div>
       )
     })
   );
@@ -49,26 +59,36 @@ class Layout extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      bestSimulations: [1, 3, 2, 1],
+      bestSimulations: [1, 3, 2, 1, 3, 4, 1],
+      recentSimulations: [3, 1, 2, 1, 5, 1, 3],
       currentSimulation: 1
     };
   }
 
   simulationChoiceHandler(sim_id) {
-      this.setState({
-          currentSimulation : sim_id
-      });
+    this.setState({
+      currentSimulation: sim_id
+    });
   }
 
   render() {
     return (
       <div className="container">
               <div className="columns">
-                <div className="column is-three-quarters is-vertical-centre">
+                <div className="column is-two-thirds is-vertical-centre">
                   <MainPanel currentSimulation={this.state.currentSimulation}/>
                 </div>
                 <div id="leaderboard" className="column is-scroll">
-                  <SimulationList simulations={ this.state.bestSimulations }
+                  <h2>Leaderboard</h2>
+                  <SimulationList showIndex={ true }
+                                  simulations={ this.state.bestSimulations }
+                                  currentSimulation={this.state.currentSimulation}
+                                  onClick={ this.simulationChoiceHandler.bind(this) }/>
+                </div>
+                <div id="recent-simulations" className="column is-scroll">
+                  <h2>Recent</h2>
+                  <SimulationList showIndex={ false }
+                                  simulations={ this.state.recentSimulations }
                                   currentSimulation={this.state.currentSimulation}
                                   onClick={ this.simulationChoiceHandler.bind(this) }/>
                 </div>
