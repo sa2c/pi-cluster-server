316ae1c582151e6c257579748af085faf3044834
Mark Dawson <mark.dawson@swansea.ac.uk>
2019-10-10 12:36:18 +0100

    styling changes to simulation cards

--- cluster_queue/react/simulationlist.jsx
+++ cluster_queue/react/simulationlist.jsx
@@ -67,8 +67,8 @@ function SimulationView(props) {
                     </div>
                 </div>
                 <div className="simulation-data">
-                    <img className="simulation-capture left" src={rgb_url} />
-                    <img className="simulation-capture right" src={depth_url} />
+                    <img className="simulation-capture" src={rgb_url} />
+                    <img className="simulation-capture" src={depth_url} />
                 </div>
             </div>
         );
