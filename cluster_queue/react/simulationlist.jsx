cad1d46672026af80156d4d75fb27c786afae7f6
Mark Dawson <mark.dawson@swansea.ac.uk>
2019-10-10 13:52:11 +0100

    whitespace changes

--- cluster_queue/react/simulationlist.jsx
+++ cluster_queue/react/simulationlist.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Avatar } from './avatar.jsx'
+import {
+  Avatar
+} from './avatar.jsx'
 
 import css from '../assets/styles/simulation-list.sass'
 
 function SimulationList(props) {
-    const simulations = props.simulations.map((sim, index) => {
-        var isCurrent = sim['id'] == props.currentSimulation
+  const simulations = props.simulations.map((sim, index) => {
+    var isCurrent = sim['id'] == props.currentSimulation
 
-        return <SimulationView key={sim['id']}
+    return <SimulationView key={sim['id']}
                                simulation={sim}
                                showIndex={props.showIndex}
                                index={index + 1}
@@ -17,56 +19,60 @@ function SimulationList(props) {
                                isCurrent={isCurrent}
                                clickHandler={props.clickHandler}/>;
 
-    });
-    return (
-        <div className="simulation-pane pane">
+  });
+
+  return (
+    <div className="simulation-pane pane">
             <h1 className="title has-text-centered">{ props.title }</h1>
             {simulations}
         </div>
-    )
+  )
 
 }
 
 function SimulationView(props) {
 
-    const simulation = props.simulation;
+  const simulation = props.simulation;
 
-    if (simulation == undefined) {
-        return null;
-    } else {
-        const sim_id = simulation['id'];
+  if (simulation == undefined) {
+    return null;
+  } else {
+    const sim_id = simulation['id'];
 
-        const rgb_url = "simulations/" + sim_id +
-                          "/rgb_with_contour.png";
-        const depth_url = "simulations/" + sim_id +
-                        "/depth.png";
+    const rgb_url = "simulations/" + sim_id +
+      "/rgb_with_contour.png";
+    const depth_url = "simulations/" + sim_id +
+      "/depth.png";
 
-        var progressBar = null;
+    var progressBar = null;
 
-        if (props.percentageKey in simulation) {
-            progressBar =
-                <div className="progress-indicator-container">
+    if (props.percentageKey in simulation) {
+      progressBar =
+        <div className="progress-indicator-container">
                     <div className="progress-indicator" style={{width : simulation.progress + "%"}}/>
                 </div>
-        }
+    }
 
-        // (slightly hacky) setting of simulation name if not provided
-        var simulation_name
-        if(simulation['name']) {
-            simulation_name = simulation['name']
-        } else {
-            simulation_name = 'Simulation'
-        }
+    // (slightly hacky) setting of simulation name if not provided
+    var simulation_name
+    if (simulation['name']) {
+      simulation_name = simulation['name']
+    } else {
+      simulation_name = 'Simulation'
+    }
 
-        // Add index to title if showIndex is true
-        if(props.showIndex) {
-            simulation_name = '#' + props.index + ' ' + simulation_name
-        }
+    // Add index to title if showIndex is true
+    if (props.showIndex) {
+      simulation_name = '#' + props.index + ' ' + simulation_name
+    }
 
-        return (
-            <div className={"simulation-view" + (props.isCurrent ? " selected" : "")}
-            onClick={() => props.clickHandler(simulation['id'])}>
-                <div className="simulation-heading">
+    const outlineColour = simulation['color']
+
+    return (
+      <div className={"simulation-view" + (props.isCurrent ? " selected" : "")}
+            onClick={() => props.clickHandler(simulation['id'])}
+            style={{ "color" : outlineColour}} >
+        <div className="simulation-heading">
                     <Avatar whom={simulation.avatar} />
                     <div className="right">
                         <div className="sim-title"> {simulation_name} </div>
@@ -78,8 +84,10 @@ function SimulationView(props) {
                     <img className="simulation-capture" src={depth_url} />
                 </div>
             </div>
-        );
-    }
+    );
+  }
 }
 
-export { SimulationList };
+export {
+  SimulationList
+};
