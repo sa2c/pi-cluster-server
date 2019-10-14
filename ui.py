477926d017680094e26a68085a008bf970ab38ab
Mark Dawson <mark.dawson@swansea.ac.uk>
2019-10-14 13:06:54 +0100

    remove results window from ui.py

--- ui.py
+++ ui.py
@@ -1,9 +1,8 @@
 from PySide2.QtCore import *
 from PySide2.QtGui import *
 from PySide2.QtWidgets import *
-import cv2, sys, time, os
+import sys, os
 import numpy as np
-from kinectlib.kinectlib import *
 from display.detail_form import DetailForm
 from display.video_capture import VideoCaptureThread, QVideoWidget
 from display.control_window import ControlWindow
@@ -12,8 +11,6 @@ from display.activity_monitor import ActivityPlotter
 from display.viewfinder import ViewfinderWindow
 from display.results_window import ResultsWindow
 from controller import Controller
-from cluster_notify import ClusterNotifyThread
-import os
 
 from settings import local_path
 
@@ -23,8 +20,6 @@ if __name__ == '__main__':
     #app.setStyle(QStyleFactory.create("Fusion"))
     # initialise another thread for video capture
 
-    cluster_notify = ClusterNotifyThread()
-
     video_source = VideoCaptureThread()
 
     controller = Controller()
@@ -33,14 +28,9 @@ if __name__ == '__main__':
 
     window = ControlWindow(controller, viewfinder, video_source)
 
-    results = ResultsWindow(cluster_notify)
-
     video_source.setParent(window)
     video_source.start()
 
-    cluster_notify.start()
-
     window.show()
     viewfinder.show()
-    results.show()
     sys.exit(app.exec_())
