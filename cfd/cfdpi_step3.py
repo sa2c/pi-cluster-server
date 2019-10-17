a0492ca7120fa61fa4d71cd1540f5e2ccae85800
pi cluster user <pi@picluster>
2019-10-17 17:51:26 +0100

    use python3 to run cfd command

--- cfd/cfdpi_step3.py
+++ cfd/cfdpi_step3.py
@@ -1,4 +1,3 @@
-
 import os
 import numpy as np
 import random
@@ -11,6 +10,7 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import model
 
+
 # Generates a random point inside a polygon
 #
 def get_random_point_in_polygon(poly):
@@ -19,6 +19,8 @@ def get_random_point_in_polygon(poly):
         p = Point(random.uniform(minx, maxx), random.uniform(miny, maxy))
         if poly.contains(p):
             return p
+
+
 ##################################################
 ##################################################
 
@@ -37,7 +39,7 @@ def generate_polyline_from_outline(sim_id, domain_area):
 
     num_points = len(outline_coords)
 
-    if(num_points <=0):
+    if (num_points <= 0):
         print("Number of points in the outline is zero\n")
         print("Program aborted")
         exit(302)
@@ -46,70 +48,74 @@ def generate_polyline_from_outline(sim_id, domain_area):
 
     mypoly = Polygon(outline_coords)
 
-
     # Compute the bounds of the outline
     (minx, miny, maxx, maxy) = mypoly.bounds
 
     lenx = maxx - minx
     leny = maxy - miny
 
-    pt1x = minx - 2.0*lenx
-    pt1y = miny - 1.0*leny
+    pt1x = minx - 2.0 * lenx
+    pt1y = miny - 1.0 * leny
 
-    pt2x = maxx + 4.0*lenx
-    pt2y = maxy + 1.0*leny
+    pt2x = maxx + 4.0 * lenx
+    pt2y = maxy + 1.0 * leny
 
-    domain_area = (pt2x-pt1x)*(pt2y-pt1y)
-
+    domain_area = (pt2x - pt1x) * (pt2y - pt1y)
 
     # Create and write the .poly file to be used for generating the mesh
 
-    poly_fname = '{run_dir}/simulation.poly'.format(run_dir=model.run_directory(sim_id))
+    poly_fname = '{run_dir}/simulation.poly'.format(
+        run_dir=model.run_directory(sim_id))
 
     poly_file = open(poly_fname, "w")
 
     # Header - <Number of poiints> <DIM> <Flag> <Flag>
-    poly_file.write("%d \t %d \t %d \t %d\n" % (num_points+4, 2, 0, 1) )
+    poly_file.write("%d \t %d \t %d \t %d\n" % (num_points + 4, 2, 0, 1))
 
     # Channel boundary
     poly_file.write("#channel boundary\n")
 
-    poly_file.write("%d \t %10.6f \t %10.6f \t %d\n" % (1, pt1x, pt1y, 2) )
-    poly_file.write("%d \t %10.6f \t %10.6f \t %d\n" % (2, pt2x, pt1y, 3) )
-    poly_file.write("%d \t %10.6f \t %10.6f \t %d\n" % (3, pt2x, pt2y, 2) )
-    poly_file.write("%d \t %10.6f \t %10.6f \t %d\n" % (4, pt1x, pt2y, 4) )
-
+    poly_file.write("%d \t %10.6f \t %10.6f \t %d\n" % (1, pt1x, pt1y, 2))
+    poly_file.write("%d \t %10.6f \t %10.6f \t %d\n" % (2, pt2x, pt1y, 3))
+    poly_file.write("%d \t %10.6f \t %10.6f \t %d\n" % (3, pt2x, pt2y, 2))
+    poly_file.write("%d \t %10.6f \t %10.6f \t %d\n" % (4, pt1x, pt2y, 4))
 
     # Immersed object
     poly_file.write("#immersed object boundary\n")
-    imm_body_id=11
+    imm_body_id = 11
     for i in range(num_points):
-        poly_file.write("%d \t %10.6f \t %10.6f \t %d\n" % ((5+i), outline_coords[i,0], outline_coords[i,1], imm_body_id) )
-
+        poly_file.write(
+            "%d \t %10.6f \t %10.6f \t %d\n" %
+            ((5 + i), outline_coords[i, 0], outline_coords[i, 1], imm_body_id))
 
     # Write edges - <count> <point #1> <point #2> <id num>
     poly_file.write("#edges\n")
-    poly_file.write("%d \t %d \n" % (num_points+4, 1) )
+    poly_file.write("%d \t %d \n" % (num_points + 4, 1))
     poly_file.write("#channel boundary\n")
-    poly_file.write("%d \t %d \t %d \t %d\n" % (1, 1, 2, 2) )
-    poly_file.write("%d \t %d \t %d \t %d\n" % (2, 2, 3, 3) )
-    poly_file.write("%d \t %d \t %d \t %d\n" % (3, 3, 4, 2) )
-    poly_file.write("%d \t %d \t %d \t %d\n" % (4, 4, 1, 4) )
+    poly_file.write("%d \t %d \t %d \t %d\n" % (1, 1, 2, 2))
+    poly_file.write("%d \t %d \t %d \t %d\n" % (2, 2, 3, 3))
+    poly_file.write("%d \t %d \t %d \t %d\n" % (3, 3, 4, 2))
+    poly_file.write("%d \t %d \t %d \t %d\n" % (4, 4, 1, 4))
 
     poly_file.write("#immersed object boundary\n")
-    imm_body_id=11
-    for i in range(num_points-1):
-        poly_file.write("%d \t %d \t %d \t %d\n" % ((5+i), (5+i), (6+i), imm_body_id) )
-    poly_file.write("%d \t %d \t %d \t %d\n" % (num_points+4, num_points+4, 5, imm_body_id) )
+    imm_body_id = 11
+    for i in range(num_points - 1):
+        poly_file.write("%d \t %d \t %d \t %d\n" % ((5 + i), (5 + i),
+                                                    (6 + i), imm_body_id))
+    poly_file.write("%d \t %d \t %d \t %d\n" %
+                    (num_points + 4, num_points + 4, 5, imm_body_id))
 
     poly_file.write("#holes\n")
     poly_file.write("1\n")
     point_in_poly = get_random_point_in_polygon(mypoly)
-    poly_file.write("%d \t %10.6f \t %10.6f \n" % (1, point_in_poly.x, point_in_poly.y) )
+    poly_file.write("%d \t %10.6f \t %10.6f \n" %
+                    (1, point_in_poly.x, point_in_poly.y))
 
     poly_file.close()
-    
+
     return
+
+
 ##################################################
 ##################################################
 
